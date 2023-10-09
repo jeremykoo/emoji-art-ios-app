b243e456f4939c945e95c79c3c3b62f8cbef10ab
Jeremy Koo <skykoo2000@gmail.com>
2023-10-09 15:34:42 -0700

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Jeremy Koo on 10/9/23.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
